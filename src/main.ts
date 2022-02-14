e41c779505caf966a3f8a8487e74280b2c82d1f2
Liupaperbox <46294886+Rorical@users.noreply.github.com>
2022-02-14 22:14:11 +0800

    feat: initial structure

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,29 @@
-export default {};
+import { QuaManager, useStore, QuaStore } from 'quastore';
+import mitt from 'mitt';
+import { Emitter, EventType, Handler } from 'mitt';
+
+const EventToPipelineType = "<UIEVENT>"
+
+type ScopeEvent = Record<EventType, unknown>;
+type UIEvent = Record<EventType, unknown>;
+
+class Pipeline {
+    handler(event: ScopeEvent) {
+
+    }
+}
+
+class QuaEngine {
+    private engineBus: Emitter<ScopeEvent>;
+    uiBus: Emitter<UIEvent>;
+    store: QuaStore;
+    private pipeline: Pipeline;
+    constructor () {
+        this.pipeline = new Pipeline();
+        this.engineBus = mitt<ScopeEvent>();
+        
+        this.uiBus = mitt<UIEvent>();
+    }
+}
+
+export default QuaEngine;
